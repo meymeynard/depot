8a464998f908c400dc6dbf52e09d8da702e7402c
meymeynard <meymeynard@gmail.com>
2021-02-16 14:02:47 +0800

    Fix syntax error

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
-  validates :image_url, allow_blank: true, format {
+  validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
     message: "Must be a URL for GIF, JPG or PNG image."
   }
